66e47e86999b33abfbe8979b3ebdc7ee7dc92c45
Felipe <luizfeliperz@gmail.com>
2018-04-02 21:11:46 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
-
+import { EnderecoService } from './services/endereco.service';
+import { HttpClientJsonpModule } from '@angular/common/http/src/module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+import { EnderecoComponent } from './services/endereco/endereco.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnderecoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+    
   ],
-  providers: [],
+  providers: [EnderecoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/endereco/endereco.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Endereco } from '../../domain/endereco';
+import { EnderecoService } from '../endereco.service';
+import { log } from 'util';
+
+@Component({
+  selector: 'app-endereco',
+  templateUrl: './endereco.component.html',
+  styleUrls: ['./endereco.component.css']
+})
+export class EnderecoComponent implements OnInit {
+
+  cep : string;
+  endereco : Endereco;
+  
+  constructor(private enderecoService : EnderecoService) { }
+
+  ngOnInit() {
+  }
+
+  getCep(){
+    this.enderecoService.getEndereco(this.cep)
+      .subscribe(response=> {
+        this.endereco = response;
+        console.log(this.endereco);
+
+      })
+  } 
+
+}
